5b8fe57383345faf3c8542ca00df93d5d69de05d
victor <victor_nvs_015@hotmail.com>
2015-04-16 17:09:20 +0200

    Primer validez del proyecto

--- Colegio/src/ejemplo1/Alumno.java
+++ Colegio/src/ejemplo1/Alumno.java
@@ -2,4 +2,30 @@ package ejemplo1;
 
 public class Alumno {
 
+	protected String nombre;
+	protected int edad;
+	protected String apellido;
+	
+	public Alumno(String a, int b) {
+		nombre=a;
+		edad=b;
+	}
+	
+	public Alumno(String a, int b, String c){
+		nombre=a;
+		edad=b;
+		apellido=c;
+	}
+	public int getedad(){
+		return edad;
+	}
+	public String getnombre(){
+		return nombre;
+	}
+	public String getapellido(){
+		return apellido;
+	}
+	
+	
+	
 }
---
+++ Colegio/src/ejemplo1/Profesor.java
@@ -0,0 +1,21 @@
+package ejemplo1;
+
+public class Profesor {
+
+	private String nomProfesor;
+	private String apeProfesor;
+	
+	public String getNomProfesor() {
+		return nomProfesor;
+	}
+	public void setNomProfesor(String nomProfesor) {
+		this.nomProfesor = nomProfesor;
+	}
+	public String getApeProfesor() {
+		return apeProfesor;
+	}
+	public void setApeProfesor(String apeProfesor) {
+		this.apeProfesor = apeProfesor;
+	}
+	
+}
